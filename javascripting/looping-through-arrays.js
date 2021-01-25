b705538c7f01dd6696d8bcb899538a700cc07bca
ben <noahbenaas@gmail.com>
2021-01-25 15:54:05 +0100

    done

---
+++ javascripting/looping-through-arrays.js
@@ -0,0 +1,10 @@
+const pets = ['cat', 'dog', 'rat']
+
+// we are putting a s in the end of each animal in the array
+for (let i = 0; i < pets.length; i++) {
+    pets[i] = pets[i] + 's'
+}
+
+console.log(pets)
+
+
---
+++ javascripting/object-keys.js
@@ -0,0 +1,11 @@
+const car = {
+    make: 'Honda',
+    model: 'Accord',
+    year: 2020
+}
+
+// this is to get the make: model: and also year not the result , thats what object does 
+const keys = Object.keys(car)
+
+console.log(keys);
+
